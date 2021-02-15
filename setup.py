02f05252aeb6552edd9195a2d3fea004caea40fc
Calum Macdonald <calum.macdonald@sheffield.ac.uk>
2021-02-15 11:53:54 +0000

    renaming yet again

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+    
 setuptools.setup(
-    name="co-connect-etltools", 
+    name="co-connect", 
     version="0.0.1",
     author="CO-CONNECT",
     author_email="CO-CONNECT@dundee.ac.uk",
@@ -13,6 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/CO-CONNECT/etl-tools",
     packages=setuptools.find_packages(),
+    install_requires=required,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
