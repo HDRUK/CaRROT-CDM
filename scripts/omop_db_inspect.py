5741532df8ec08b1e7c3979eaa89dad95913127b
Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
2021-03-17 13:42:52 +0000

    Format print statements

--- scripts/omop_db_inspect.py
+++ scripts/omop_db_inspect.py
@@ -31,35 +31,39 @@ omop_tables.sort()
 print (json.dumps(omop_tables,indent=6))
 
 for _id in ['8507','378253','40305063','4060225']:
-    selection1 = r'''
+    select_from_concept = r'''
      SELECT *
      FROM public.concept
      WHERE concept_id=%s
     '''
-    selection2 = r'''
+    select_from_concept_relationship = r'''
     SELECT *
     FROM public.concept_relationship
     WHERE concept_id_1=%s
     '''
-    df = pd.read_sql(selection1%(_id),ngin).drop(["valid_start_date","valid_end_date","invalid_reason"],axis=1)
-    df2 = pd.read_sql(selection2%(_id),ngin).drop(["valid_start_date","valid_end_date","invalid_reason"],axis=1)
+    df_concept = pd.read_sql(select_from_concept%(_id),ngin).drop(["valid_start_date","valid_end_date","invalid_reason"],axis=1)
+    df_relationship = pd.read_sql(select_from_concept_relationship%(_id),ngin).drop(["valid_start_date","valid_end_date","invalid_reason"],axis=1)
     print ()
-    print (df.T)
+    print (df_concept.T)
     print ()
-    print (df2.T)
-    relationships=df2['relationship_id'].tolist()
+    print (df_relationship.T)
+    relationships=df_relationship['relationship_id'].tolist()
     for relationship in relationships:
         if relationship=="Mapped from":
             is_standard="Standard"
-            concept_id=df2['concept_id_1'].iloc[relationships.index(relationship)]
-            destination_table = df['domain_id'].iloc[relationships.index(relationship)]
+            concept_id=df_relationship['concept_id_1'].iloc[relationships.index(relationship)]
+            source_concept_id=concept_id
+            destination_table = df_concept['domain_id'].iloc[relationships.index(relationship)]
         elif relationship=="Concept same_as to":
             is_standard="Non-Standard"
-            non_standard_concept_id=df2['concept_id_1'].iloc[relationships.index(relationship)]
-            concept_id=df2['concept_id_2'].iloc[relationships.index(relationship)]
-            destination_table = df['domain_id'].iloc[0]
+            source_concept_id=df_relationship['concept_id_1'].iloc[relationships.index(relationship)]
+            concept_id=df_relationship['concept_id_2'].iloc[relationships.index(relationship)]
+            
+            destination_table = df_concept['domain_id'].iloc[0]
+    print()
     print("Mapping is: ",is_standard)
-    print("Stardard Mapping is: ",concept_id)
+    print("concept_id is: ",concept_id)
+    print("source_concept_id is",source_concept_id)
     print ("Destination Table is:",destination_table)
 exit(0)
 
