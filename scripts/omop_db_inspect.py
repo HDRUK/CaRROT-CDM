3984097865160dd3ff2f491c963eefca6f7948d3
Calum Macdonald <calum.macdonald@sheffield.ac.uk>
2021-03-17 09:59:08 +0000

    change db direct

--- scripts/omop_db_inspect.py
+++ scripts/omop_db_inspect.py
@@ -6,6 +6,7 @@ import pandas as pd
 from dotenv import load_dotenv
 load_dotenv()
 
+
 db_name = os.environ['POSTGRES_DB']
 db_user = os.environ['POSTGRES_USER']
 db_password = os.environ['POSTGRES_PASSWORD']
@@ -29,37 +30,89 @@ omop_tables.sort()
 
 print (json.dumps(omop_tables,indent=6))
 
- 
+
+for _id in ['8507','8532','4057304','36716287','37398192','378253']:
+    selection = r'''
+    SELECT *
+    FROM public.concept
+    WHERE concept_id=%s
+    '''
+    df = pd.read_sql(selection%(_id),ngin).drop(["valid_start_date","valid_end_date","invalid_reason"],axis=1)
+    print ()
+    print (df.T)
+    class_id = df['concept_class_id'].iloc[0]
+    domain_id = df['domain_id'].iloc[0]
+    #print (class_id)
+    #print (domain_id)
+exit(0)
+
 selection = r'''
 SELECT *
-FROM public.concept
-WHERE (concept_id=8507 OR concept_id=8532)
-'''#8532
-df = pd.read_sql(selection,ngin).drop(['valid_start_date','valid_end_date','invalid_reason'],axis=1)
+FROM public.concept_synonym
+WHERE concept_id=%s
+'''# 8507 8532
+df = pd.read_sql(selection%('8507'),ngin)
 print (df)
+
+selection = r'''
+SELECT *
+FROM public.domain
+WHERE domain_id LIKE '%s'
+'''%(f'%%{domain_id}%%')
+df = pd.read_sql(selection,ngin)
+print (df)
+
+
+selection = r'''
+SELECT *
+FROM public.location_history
+'''
+df = pd.read_sql(selection,ngin)
+print (df)
+exit(0)
+
+
+
+selection = r'''
+SELECT *
+FROM public.concept_class
+WHERE concept_class_id LIKE '%s';
+'''%(f"%%{class_id}%%")
+
+df = pd.read_sql(selection,ngin)
+print (df.columns)
+print (df)
+print ()
+
 
 for table in filter(lambda tab: 'concept' in tab, omop_tables):
     selection = r'''
     SELECT *
     FROM public.%s
-    LIMIT 10;
+    LIMIT 1
     '''%(table)
-    print (table)
     df = pd.read_sql(selection,ngin)
-    print (df.columns)
-    print (df)
-    print ()
+    print ({table:df.columns})
 
-exit(0)
+
 
 def get_full_df(table,schema=schema):
     selection = r'''
     SELECT *
     FROM %s.%s
+    LIMIT 10
     '''%(schema,table)
     return pd.read_sql(selection,ngin)
 
-print (get_full_df('person').columns.values)
+#print (get_full_df('person').columns.values)
+#print (get_full_df('source_to_concept_map'))
+
+dict={}
+for table in omop_tables:
+    dict[table] = list(get_full_df(table).columns.values)
+
+print (json.dump(dict,open('info.json','w'),indent=6))
+
 
 
 
