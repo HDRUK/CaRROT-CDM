6d1d1855d4110639fe3cabd25427192f0cce4988
Calum Macdonald <calum.macdonald@sheffield.ac.uk>
2021-03-23 13:42:46 +0000

    improving omop_db_isnpect

--- coconnect/tools/omop_db_inspect.py
+++ coconnect/tools/omop_db_inspect.py
@@ -2,6 +2,7 @@ import json
 import os
 import sqlalchemy as sql
 import pandas as pd
+import numpy as np
 
 class OMOPDetails():
     def __init__(self):
@@ -17,28 +18,42 @@ class OMOPDetails():
 
         self.inspector = sql.inspect(self.ngin)
         self.schema = 'public'
-        self.omop_tables = [
-            table
-            for table in self.inspector.get_table_names(schema=self.schema)
-        ]
-        self.omop_tables.sort()
-        print (json.dumps(self.omop_tables,indent=6))
 
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        _version = 'v5_3_1'
+        f_path = f'{dir_path}/../data/cdm/OMOP_CDM_{_version}.csv'
+        self.cdm = pd.read_csv(f_path,encoding="ISO-8859-1")\
+                     .set_index('table')[['field']]
+        
+        #self.omop_tables = [
+        #    table
+        #    for table in self.inspector.get_table_names(schema=self.schema)
+        #]
+        #self.omop_tables.sort()
 
-    def get_target_concept_id_and_table(self,source_concept_id):
+    def get_rules(self,source_concept_ids):
         #From OMOP db get concept relationship
         select_from_concept = r'''
         SELECT *
         FROM public.concept
-        WHERE concept_id=%s
+        WHERE concept_id IN (%s)
         '''
         select_from_concept_relationship = r'''
         SELECT *
         FROM public.concept_relationship
-        WHERE concept_id_1=%s
+        WHERE concept_id_1 IN (%s)
         '''
+        
+        if isinstance(source_concept_ids,int):
+            source_concept_ids = {None: source_concept_ids}
+            
+        _ids = ",".join([
+            str(x)
+            for x in source_concept_ids.values()
+        ])
+        
         df_concept = pd.read_sql(
-            select_from_concept%(source_concept_id),self.ngin)\
+            select_from_concept%(_ids),self.ngin)\
                        .drop(
                            [
                                "valid_start_date",
@@ -46,8 +61,76 @@ class OMOPDetails():
                                "invalid_reason"
                            ]
                            ,axis=1)
-        return df_concept
-#         df_relationship = pd.read_sql(select_from_concept_relationship%(source_concept_id),ngin).drop(["valid_start_date","valid_end_date","invalid_reason"],axis=1)
+        df_relationship = pd.read_sql(
+            select_from_concept_relationship%(_ids),self.ngin)\
+                            .drop(
+                                ["valid_start_date",
+                                 "valid_end_date",
+                                 "invalid_reason"],axis=1)
+
+        relationship_ids = [
+            #'Mapped from',
+            'Concept same_as to',
+            'Maps to']
+        df_relationship = df_relationship[
+            df_relationship['relationship_id'].isin(relationship_ids)
+        ]
+        df_concept.set_index('concept_id',inplace=True)
+        df_relationship.set_index('concept_id_1',inplace=True)
+
+        
+        info = df_concept.join(df_relationship).reset_index()
+        info['domain_id'] = info['domain_id'].str.lower()
+        domains = info['domain_id'].unique()
+        #cond = self.cdm['field'].str.contains(domain) \
+        #    & self.cdm['field'].str.contains('concept_id')
+
+        cols = ['concept_id', 'concept_name', 'domain_id', 'vocabulary_id',
+                'concept_class_id', 'standard_concept', 'concept_code',
+                'concept_id_2','relationship_id']
+        info = info[['concept_id','concept_id_2','domain_id']]
+        info.set_index('domain_id',inplace=True)
+        info.columns = ['source_concept_id','concept_id']
+
+        temp = pd.DataFrame.from_dict(source_concept_ids,
+                                      columns=['source_concept_id'],
+                                      orient='index')
+        temp.index.rename('source_value',inplace=True)
+        temp.reset_index(inplace=True)
+        info = info.reset_index().merge(temp,
+                                        left_on='source_concept_id',
+                                        right_on='source_concept_id')\
+                                 .set_index('domain_id')
+
+        domain_id = info.index.unique()[0]
+
+        if len(info.index.unique()) > 1:
+            print ('fuck off')
+            exit(0)
+
+        info.columns = [f"{domain_id}_{col}" for col in info.columns]
+        temp = info.loc[[domain_id]]\
+                   .reset_index(drop=True)\
+                   .set_index(f"{domain_id}_source_value")\
+                   .T\
+                   .astype('Int64')\
+                   .fillna(np.NaN)\
+                   .astype(str)
+        
+        temp = {
+            x:temp.loc[x].to_dict()
+            for x in temp.index
+        }
+
+        #convert None to scalar for field level mapping
+        for k,v in temp.items():
+            if None in v:
+                temp[k] = v[None]
+        
+        temp[f"{domain_id}_source_value"] = None
+        return temp
+
+        
 #         relationships=df_relationship['relationship_id'].tolist()
 #         #1)Check if source_concept_id is Standard or Non-standard
 #         #2)Get the relevant target table for the source_concept_id
@@ -63,86 +146,18 @@ class OMOPDetails():
 #                 self.target_concept_id=df_relationship['concept_id_2'].iloc[relationships.index(relationship)]
 #                 self.target_table = df_concept['domain_id'].iloc[0]
 
-# concept_id=4060225
-# detail1=OMOPDetails(concept_id)
-# print(detail1.target_concept_id)
-# print(detail1.target_table)
-# print(detail1.is_standard)
-# exit(0) 
-        
+if __name__ == '__main__':
+    # concept_id=4060225
+    from dotenv import load_dotenv
+    load_dotenv()
+    tool = OMOPDetails()
+    print (tool.get_rules(37399052))
+    exit(0)
+    print (tool.get_rules({'M':8507,'F':8532}))
+    print (tool.get_rules({"BLACK CARIBBEAN": 4087917, "ASIAN OTHER": 4087922, "INDIAN": 4185920, "WHITE BRITISH": 4196428}))
 
-# selection = r'''
-# SELECT *
-# FROM public.concept_synonym
-# WHERE concept_id=%s
+    print (tool.get_rules({'0.2':37398191,'0.4':37398191}))
+    
+    
+    #print (tool.get_info(4060225))
 # '''# 8507 8532
-# df = pd.read_sql(selection%('8507'),ngin)
-# print (df)
-
-# selection = r'''
-# SELECT *
-# FROM public.domain
-# WHERE domain_id LIKE '%s'
-# '''%(f'%%{domain_id}%%')
-# df = pd.read_sql(selection,ngin)
-# print (df)
-
-
-# selection = r'''
-# SELECT *
-# FROM public.location_history
-# '''
-# df = pd.read_sql(selection,ngin)
-# print (df)
-# exit(0)
-
-
-
-# selection = r'''
-# SELECT *
-# FROM public.concept_class
-# WHERE concept_class_id LIKE '%s';
-# '''%(f"%%{class_id}%%")
-
-# df = pd.read_sql(selection,ngin)
-# print (df.columns)
-# print (df)
-# print ()
-
-
-# for table in filter(lambda tab: 'concept' in tab, omop_tables):
-#     selection = r'''
-#     SELECT *
-#     FROM public.%s
-#     LIMIT 1
-#     '''%(table)
-#     df = pd.read_sql(selection,ngin)
-#     print ({table:df.columns})
-
-
-
-# def get_full_df(table,schema=schema):
-#     selection = r'''
-#     SELECT *
-#     FROM %s.%s
-#     LIMIT 10
-#     '''%(schema,table)
-#     return pd.read_sql(selection,ngin)
-
-# #print (get_full_df('person').columns.values)
-# #print (get_full_df('source_to_concept_map'))
-
-# dict={}
-# for table in omop_tables:
-#     dict[table] = list(get_full_df(table).columns.values)
-
-# print (json.dump(dict,open('info.json','w'),indent=6))
-
-
-
-
-# #omop_fields = {
-# #    table: get_df(table).columns
-# #    for table in omop_tables
-# #}
-# #print (omop_fields)
