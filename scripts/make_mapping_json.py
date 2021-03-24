fcbae9117d27156946dee17a3e2ccf9eae3aa317
Calum Macdonald <calum.macdonald@sheffield.ac.uk>
2021-03-24 19:15:04 +0000

    switch pipeline helpers to work on json instead of csv

--- scripts/make_mapping_json.py
+++ scripts/make_mapping_json.py
@@ -4,22 +4,11 @@ from coconnect.tools import mapping_pipeline_helpers
 
 structural_mapping = mapping_pipeline_helpers\
     .StructuralMapping\
-    .to_json('University of Nottingham_PANTHER ds_structural_mapping.csv',
-             'University of Nottingham_PANTHER ds_term_mapping.csv',
+    .to_json('University of Nottingham_PANTHER_AUTO_structural_mapping.json',
              save = 'panther_structural_mapping.json',
              strict = False,
              destination_tables = ['person','condition_occurrence','visit_occurrence']
     )
-
-
-# structural_mapping = mapping_pipeline_helpers\
-#     .StructuralMapping\
-#     .to_json('MATCH/University of Dundee_MATCH_structural_mapping.csv',
-#              'MATCH/University of Dundee_MATCH_term_mapping.csv',
-#              save = 'match_test.json',
-#              strict = False,
-#              destination_tables = ['person','condition_occurrence']
-#     )
 
 
 
