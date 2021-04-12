eb99ca3d15bdd7fb96a8a1639b74fadb087180d7
Calum Macdonald <calum.macdonald@sheffield.ac.uk>
2021-04-12 14:36:09 +0100

    fix dag display for cli

--- coconnect/cdm/__init__.py
+++ coconnect/cdm/__init__.py
@@ -1,5 +1,5 @@
 from .model import CommonDataModel
 
 from .objects import Person, ConditionOccurrence, VisitOccurrence, Measurement
-from .decorators import define_person, define_condition_occurrence, define_visit_occurrence, define_measurement
+from .decorators import define_person, define_condition_occurrence, define_visit_occurrence, define_measurement, define_observation
 
