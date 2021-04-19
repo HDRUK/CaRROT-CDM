0d2a3fc4ebde48cbee94933da4c5a0027d416de1
Calum Macdonald <calum.macdonald@sheffield.ac.uk>
2021-04-19 10:41:36 +0100

    bump to v0.1.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
     
 setuptools.setup(
     name="co-connect-tools", 
-    version="0.1.7",
+    version="0.1.8",
     author="CO-CONNECT",
     author_email="CO-CONNECT@dundee.ac.uk",
     description="Python package for performing mapping of ETL to CDM ",
