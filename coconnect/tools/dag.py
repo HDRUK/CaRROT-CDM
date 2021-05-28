46a6bf6b24be7a45ff9c41bb7c0a4cd57e87df73
Calum Macdonald <calum.macdonald@sheffield.ac.uk>
2021-05-28 10:48:16 +0100

    add colours to dag

--- coconnect/tools/dag.py
+++ coconnect/tools/dag.py
@@ -21,11 +21,13 @@ def make_dag(data,render=False):
                 source_table = source['source_table']
 
                 table_name = f"{destination_field}_{source_table}"
-                
                 dot.node(table_name,
                          label=destination_field,style='filled', fillcolor='yellow',shape='box')
+
                 dot.edge(destination_table_name,table_name,dir='back')
-                
+
+
+                dot.node(source_field)
 
                 if 'operations' in source:
                     operations = source['operations']
@@ -36,7 +38,10 @@ def make_dag(data,render=False):
                     #dot.node(tmap,label=json.dumps(term_mapping),style='filled', fillcolor='azure2',shape='box')
                     #dot.edge(tmap,source_field,dir='back')
                     #dot.edge(table_name,tmap,dir='back')
+                    #for i,(k,v) in enumerate(term_mapping.items()):
+                    #    label = f'"{k}":"{v}"'
                     dot.edge(table_name,source_field,dir='back',color='red')
+                        
                 else:                                                    
                     dot.edge(table_name,source_field,dir='back')
 
