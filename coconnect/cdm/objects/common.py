c26ff16eab77689e25e6ec4994168ce81880fb52
Calum Macdonald <calum.macdonald@sheffield.ac.uk>
2021-07-29 11:00:17 +0100

    missed Text60

--- coconnect/cdm/objects/common.py
+++ coconnect/cdm/objects/common.py
@@ -27,6 +27,7 @@ class DataFormatter(collections.OrderedDict):
         super().__init__()
         self['Integer'] = lambda x : pd.to_numeric(x,errors='coerce').astype('Int64')
         self['Text50'] = lambda x : x.fillna('').astype(str).apply(lambda x: x[:50])
+        self['Text60'] = lambda x : x.fillna('').astype(str).apply(lambda x: x[:60])
         self['Timestamp'] = lambda x : pd.to_datetime(x,errors='coerce').dt.strftime('%Y-%m-%d %H:%M:%S')
         self['Date'] = lambda x : pd.to_datetime(x,errors='coerce').dt.date
 
