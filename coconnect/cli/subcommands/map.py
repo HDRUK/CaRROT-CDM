0fcfa081824bba130288f74c37ce67f6e62e7521
Calum Macdonald <calum.macdonald@sheffield.ac.uk>
2021-08-05 16:57:19 +0100

    implement a simple gui

--- coconnect/cli/subcommands/map.py
+++ coconnect/cli/subcommands/map.py
@@ -317,23 +317,59 @@ def run_pyconfig(ctx,rules,pyconf,inputs,
         raise NotImplementedError("You need to run the CLI tool with either a json or python configuration file")
 
 @click.command(help="run as a Graphical User Interface (GUI)")
-def gui():
+@click.pass_context
+def gui(ctx):
     import PySimpleGUI as sg
 
-    sg.theme('DarkAmber')   # Add a touch of color
-    # All the stuff inside your window.
-    layout = [  [sg.Text('Some text on Row 1')],
-                [sg.Text('Enter something on Row 2'), sg.InputText()],
-                [sg.Button('Ok'), sg.Button('Cancel')] ]
+    coconnect_theme = {'BACKGROUND': '#475da7',
+                       'TEXT': '#FFFFFF',
+                       'INPUT': '#c4c6e2',
+                       'TEXT_INPUT': '#000000',
+                       'SCROLL': '#c7e78b',
+                       'BUTTON': ('white', '#3DB28C'),
+                       'PROGRESS': ('#01826B', '#D0D0D0'),
+                       'BORDER': 1,
+                       'SLIDER_DEPTH': 0,
+                       'PROGRESS_DEPTH': 0}
     
-    # Create the Window
-    window = sg.Window('Window Title', layout)
-    # Event Loop to process "events" and get the "values" of the inputs
+    # Add your dictionary to the PySimpleGUI themes
+    sg.theme_add_new('coconnect', coconnect_theme)
+    sg.theme('coconnect')
+    
+    layout = [
+        [sg.T('Select the rules json:')],
+        [sg.Input(key='_RULES_'), sg.FilesBrowse()],
+        [sg.T('Select the input CSVs:')],
+        [sg.Input(key='_INPUTS_'), sg.FilesBrowse()],
+        [sg.OK('Run'), sg.Cancel()]
+    ]
+
+    font = ("Roboto", 15)
+    
+    window = sg.Window('COCONNECT', layout, font=font)
     while True:
-        event, values = window.read()
-        if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        event, values = window.Read()
+        
+        if event == 'Cancel':
             break
-        print('You entered ', values[0])
+    
+        rules = values['_RULES_']
+        if rules == '':
+            sg.Popup(f'Error: please select a rules file')
+            continue
+        elif len(rules.split(';'))>1:
+            sg.Popup(f'Error: only select one file for the rules!')
+            continue
+
+        inputs = values['_INPUTS_']
+        if inputs == '':
+            sg.Popup(f'Error: please select at least one file or directory for the inputs')
+            continue
+        
+        inputs = inputs.split(';')
+        ctx.invoke(run,rules=rules,inputs=inputs)
+        sg.Popup("Done!")
+        break
         
     window.close()
 
