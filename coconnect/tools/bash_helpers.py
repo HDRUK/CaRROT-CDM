ca8fc16aa1ee0e88ff1fb580d277d3738eeed0a2
Calum Macdonald <calum.macdonald@sheffield.ac.uk>
2022-02-02 12:40:51 +0000

    fix logger issue

--- coconnect/tools/bash_helpers.py
+++ coconnect/tools/bash_helpers.py
@@ -13,13 +13,13 @@ class BashHelpers(Logger):
             cmd = cmd.split(" ")
         elif not isinstance(cmd,list):
             raise Exception("run_bash_cmd must be passed a bash command as a str or a list")
-        self.__logger.notice(" ".join(cmd))
+        self.logger.notice(" ".join(cmd))
         if self.dry_run:
             return None,None
         session = subprocess.Popen(cmd, stdout=PIPE, stderr=PIPE)
         stdout, stderr = (x.decode("utf-8") for x in session.communicate())
 
         if 'ERROR' in stderr:
-            self.__logger.critical(stderr)
+            self.logger.critical(stderr)
             raise Exception("failled executing bash command")
         return stdout,stderr
